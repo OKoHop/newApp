6c848d4d6e6a0e1a4c007e7dbdea006a33632f9e
Nikita <example@gmail.com>
2022-05-04 20:31:11 +0300

    new version

--- main.js
+++ main.js
@@ -8,12 +8,24 @@ let numberOfFilms = +prompt('Сколько фильмов вы уже посм�
     privat: false
 };
 
-let lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
-    ratingFilm = +prompt('На сколько оцените его?', ''),
-    lastFilmX = prompt('Один из последних просмотренных фильмов?', ''),
-    ratingFilmY = +prompt('На сколько оцените его?', '');
+for (let i = 0; i <= 2; i++){
+    let lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
+        ratingFilm = +prompt('На сколько оцените его?', '');
+    if(lastFilm != null && ratingFilm != null && lastFilm != '' && ratingFilm != '' && lastFilm.length < 50){
+        personalMovieDB.movies[lastFilm] = ratingFilm;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
-personalMovieDB.movies[lastFilm] = ratingFilm;
-personalMovieDB.movies[lastFilmX] = ratingFilmY;
-
-console.log(personalMovieDB);
+if (personalMovieDB.count < 10){
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB < 30){
+    console.log('Вы классический зритель');
+}  else if (personalMovieDB.count > 30){
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
